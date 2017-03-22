5101aff5e20365bb83afecf2ccef397c8ac4a54d
Edward J Kim <edward.junhyung.kim@gmail.com>
2017-03-22 15:18:34 -0400

    save current frame; remove video feed

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import eventlet
 eventlet.monkey_patch()
 from flask import Flask, render_template, session, request, Response, redirect, url_for
 from flask_socketio import SocketIO, emit, disconnect
+
+import os
 import base64
 import re
 from PIL import Image
@@ -22,35 +24,22 @@ socketio = SocketIO(app)
 def index():
     """Video streaming home page."""
     if request.method == 'GET':
+
         return render_template('index.html', async_mode=socketio.async_mode)
+
     if request.method == 'POST':
+
         image_b64 = request.form['frame_data']
-        image_data = re.sub('^data:image/.+;base64,', '', image_b64).decode('base64')
-        with open('static/test.png', 'w') as f:
+        image_data = re.sub('^data:image/.+;base64,', '', image_b64)
+        image_data = image_data.decode('base64')
+
+        if os.path.exists('static/current_frame.png'):
+            os.rename('static/current_frame.png', 'static/previous_frame.png')
+
+        with open('static/current_frame.png', 'w') as f:
             f.write(image_data)
+
         return Response('OK')
-
-
-def gen(video):
-    """Video streaming generator function."""
-    while True:
-        frame = video.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/original_video_feed')
-def original_video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Video('original_images', 25)),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-@app.route('/upsampled_video_feed')
-def upsampled_video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Video('upsampled_images', 25)),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @socketio.on('frame', namespace='/')
