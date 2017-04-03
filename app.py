fcbd5e53ec490eef8fac9b1199737d9d3af08cac
Edward J Kim <edward.junhyung.kim@gmail.com>
2017-04-02 21:33:02 -0400

    password protect webcam

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 import base64
 import urllib
 import re
+from functools import wraps
 
 from PIL import Image
 import cStringIO
@@ -58,7 +59,33 @@ def process_frames(previous_frame, current_frame, shape=(90, 120, 3)):
     return images
 
 
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
 @app.route('/webcam', methods=['GET', 'POST'])
+@requires_auth
 def webcam():
     """Video streaming home page."""
     if request.method == 'GET':
